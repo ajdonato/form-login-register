4fb7a3d90e3c26d32f516c0362f954285c2ec711
ajdonato <ajd.oliveira@gmail.com>
2021-03-12 08:03:13 -0400

    add correction vh mobile with js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
 const container = document.querySelector(".container");
+let vh = window.innerHeight * 0.01;
 
 sign_up_btn.addEventListener('click', () => {
     container.classList.add("sign-up-mode");
@@ -8,3 +9,5 @@ sign_up_btn.addEventListener('click', () => {
 sign_in_btn.addEventListener('click', () => {
     container.classList.remove("sign-up-mode");
 })
+
+document.documentElement.style.setProperty('--vh', `${vh}px`);
